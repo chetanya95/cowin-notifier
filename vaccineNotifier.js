64450ada7e08693be838ba7c13a34126103c3796
Chetanya Goyal <chegoy@amazon.com>
2021-05-09 01:07:09 +0530

    optimization for breaking code for a entry if slot is found

--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -19,23 +19,25 @@ async function main() {
 }
 
 async function checkAvailability() {
+    let datesArray = fetchNext10Days();
 
-    let datesArray = await fetchNext10Days();
-
-    findEntries.forEach(entry => {
+    for(entry of findEntries){
         var findBy = entry.find_by;
         var findValue = entry.find_value;
         var age = entry.age;
         var toEmail = entry.to_email;
         console.log("Finding available slots for " + toEmail + " of age " + age + " by " + findBy + " " + findValue);
 
-        datesArray.forEach(date => {
-            getSlotsForDate(date, findBy, findValue, age, toEmail);
-        });
-    });
+        for(date of datesArray){
+            const isSlotFound = await getSlotsForDate(date, findBy, findValue, age, toEmail);
+            if(isSlotFound === true){
+                break;
+            }
+        }
+    };
 }
 
-function getSlotsForDate(date, findBy, findValue, age, toEmail) {
+async function getSlotsForDate(date, findBy, findValue, age, toEmail) {
     const URL_FIND_BY_PINCODE = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=' + findValue + '&date=' + date;
     const URL_FIND_BY_DISTRICT = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=' + findValue + '&date=' + date;
     var url = findBy === 'district' ? URL_FIND_BY_DISTRICT : URL_FIND_BY_PINCODE;
@@ -49,45 +51,42 @@ function getSlotsForDate(date, findBy, findValue, age, toEmail) {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
     };
 
-    fetch(url, { method: 'GET', headers: headers })
-    .then(res => res.json())
-    .then(responseJSON => {
-        //console.log("responseJSON: " + JSON.stringify(responseJSON));
-        let centers = responseJSON.centers;
+    let isSlotFound = false;
+    try{
+        const response = await fetch(url, { method: 'GET', headers: headers });
+        const responseJSON = await response.json();
+        const centers = responseJSON.centers;
         for(let center of centers){
             let sessions = center.sessions;
             //console.log('center:' + JSON.stringify(center));
             //console.log('sessions:' + JSON.stringify(sessions));
-            if(!sessions){
-                console.log("No sessions in center: " + center.name);
-                return;
+            if(sessions){
+                let validSlots = sessions.filter(session => session.min_age_limit <= age && session.available_capacity > 0);
+                console.log({ date: date, center: center.name, validSlots: validSlots.length });
+                if (validSlots.length > 0) {
+                    console.log("Valid vaccination slot(s) found for user " + toEmail + ", in center " +  center.name + ", sending an email");
+                    notifyMe(center, toEmail);
+                    isSlotFound = true;
+                    break;
+                }
             }
-            let validSlots = sessions.filter(session => session.min_age_limit <= age && session.available_capacity > 0);
-            console.log({ date: date, validSlots: validSlots.length })
-            if (validSlots.length > 0) {
-                console.log("Valid vaccination slot(s) found for user " + toEmail + ", in center " +  center.name + ", sending an email");
-                notifyMe(center, toEmail);
-                return;
-            }
-            console.log("Found no slot in center: " + center.name);
         }
-        
-    })
-    .catch(function (error) {
+    } catch (error){
         console.error(error);
-    });
+    }
+    return isSlotFound;
 }
 
 async function notifyMe(center, toEmail) {
     const details = JSON.stringify(center, null, '\t');
-    notifier.sendEmail(toEmail, 'Vaccine available on COWIN portal!', details, (err, result) => {
+    notifier.sendEmail(toEmail, 'Vaccine available on COWIN Portal!', details, (err, result) => {
         if (err) {
             console.error({ err });
         }
     })
 };
 
-async function fetchNext10Days() {
+function fetchNext10Days() {
     let dates = [];
     let today = moment();
     for (let i = 0; i < 10; i++) {
